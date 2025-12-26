dc80fd8ab26b47f001cfb9ca297f9e2d82a19d2c
dragdroperp-cyber <dragdroperp@gmail.com>
2025-12-26 12:27:19 +0530

    Delete src directory

--- src/utils/sessionCache.js
+++
@@ -1,9 +0,0 @@
-// Simple in-memory cache to track if an API call has been made during the current browser session.
-// This allows us to skip API calls on client-side navigation (using IndexedDB instead),
-// but ensures we fetch fresh data on browser refresh (F5), as this object is reset.
-
-const sessionCache = new Set();
-
-export const hasFetched = (key) => sessionCache.has(key);
-export const markFetched = (key) => sessionCache.add(key);
-export const clearSessionCache = () => sessionCache.clear();
